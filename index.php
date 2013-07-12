148b65c8c9a5f3f9400e91ace823dbc1e1bdc2eb
rayedchan <rayedchan@hotmail.com>
2013-07-11 21:39:21 -0400

    web interface for a story tree

--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.10.3.min.js"></script>
+        <script type="text/javascript" src="js/jquery.jOrgChart.js"></script>
+        <link rel="stylesheet" href="css/jquery.jOrgChart.css"/>
+        <link rel="stylesheet" href="css/custom.css"/>
+        <title>Story Tree</title>
+        <script type="text/javascript">
+            jQuery(document).ready(function() {
+                 $("#org").jOrgChart();
+            });
+        </script>
     </head>
+
     <body>
-        <?php
-        echo 'hello';
-        ?>
+        <ul id="org" style="display:none">
+            <li><a href="#" target="_blank">Chapter 1</a><br /><font size="1px"><i>Author: Ray</i></font>     
+                <ul> 
+                    <li><a href="#" target="_blank">Chapter 2a</a></li>
+                    <li><a href="#" target="_blank">Chapter 2b</a>
+                        <ul>
+                          <li><a href="#" target="_blank">Chapter 3a</a></li>
+                          <li><a href="#" target="_blank">Chapter 3b</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#" target="_blank">Chapter 2c</a><br /><font size="1px"><i>Author: Anonymous</i></font></li>
+                    <li><a href="#" target="_blank">Chapter 2d</a>
+                        <ul>
+                          <li><a href="#" target="_blank">Chapter 3c</a></li>
+                          <li><a href="#" target="_blank">Chapter 3d</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
     </body>
 </html>
