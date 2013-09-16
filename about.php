8d00caa0b99c1136a739ee1af9f4336451ce3f7f
rayedchan <rayedchan@hotmail.com>
2013-09-16 18:59:04 -0400

    -Rename jOrgChart custom css to a more meaningful name

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="css/jquery.jOrgChart.css"/>
-        <link rel="stylesheet" href="css/custom.css"/>
+        <link rel="stylesheet" href="css/jquery.jOrgChart.customizable.css"/>
         <link rel="stylesheet" href="css/mycustom.css"/>
         <title>About</title>
     </head>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
         <script type="text/javascript" src="js/jquery-ui-1.10.3.min.js"></script>
         <link rel="stylesheet" href="css/jquery.jOrgChart.css"/>
-        <link rel="stylesheet" href="css/custom.css"/>
+        <link rel="stylesheet" href="css/jquery.jOrgChart.customizable.css"/>
         <link rel="stylesheet" href="css/mycustom.css"/>
         <title>Story Tree</title>
     </head>
