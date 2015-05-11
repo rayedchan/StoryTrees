ecc4ae6913c00d7bf4824e2c532f3fa5d330455f
rayedchan <rayedchan@hotmail.com>
2015-05-10 21:13:24 -0400

    Removed PostgreSQL connection method

--- classes/dbconnection.php
+++ classes/dbconnection.php
@@ -92,34 +92,6 @@ class DbConnection
     {
         return $this->connection_resource;
     }
-    
-    /*
-     * Connects to a PostgreSQL Database  
-     */
-    private function connectToPostgreSQLDB()
-    {
-        //Connection Settings for PostgreSQL DB; connectionString format KEYWORD=VALUE
-        $hostname = 'host=ec2-54-235-134-222.compute-1.amazonaws.com';
-        $port = 'port=5432';
-        $dbname = 'dbname=dev2jctlvph8it';
-        $dbuser = 'user=pbsvyowsqpmzzu';
-        $dbpassword = 'password=hvTStiJa-pjszB6GfyxVbKQjfN';
-        $mode = "sslmode=require";
-        $connection_string = "$hostname $port $dbname $mode $dbuser $dbpassword";
-
-        $dbconnection_resource = pg_connect($connection_string); //Connect to PostgreSQL database
-        if(!dbconnection_resource)
-             die("Could not connect: " . pg_last_error($dbconnection_resource));
-        echo 'Connected Successfully.';
-    }
-
-    /*
-     * Close PostgreSQL Database connection
-     */
-    public function closePostgreSQLConnection($dbconnection_resource)
-    {
-        pg_close($dbconnection_resource);//Close database connection
-    }
 }
 
 function quickMySQLConnect()
